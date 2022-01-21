0dfa9b6645cb4f2b3a3a6691c906250dbac353a5
Liu Jiang <gerry@linux.alibaba.com>
2022-01-21 22:34:17 +0800

    db-arch: refine documentation
    Refine documentation for db-arch.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- crates/db-arch/src/aarch64/mod.rs
+++ crates/db-arch/src/aarch64/mod.rs
@@ -1,2 +1,4 @@
 // Copyright 2021 Alibaba Cloud. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+
+//! CPU architecture specific constants and utilities for the `aarch64` architecture.
--- crates/db-arch/src/lib.rs
+++ crates/db-arch/src/lib.rs
@@ -1,8 +1,21 @@
-// Copyright 2021 Alibaba Cloud. All Rights Reserved.
+// Copyright 2021-2022 Alibaba Cloud. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+
+#![deny(missing_docs)]
+
+//! CPU architecture specific constants and utilities.
+//!
+//! This crate provides CPU architecture specific constants and utilities to abstract away CPU
+//! architecture specific details from the Dragonball Sandbox or other VMMs.
+//!
+//! # Supported CPU Architectures
+//! - **x86_64**: x86_64 (also known as x64, x86-64, AMD64, and Intel 64) is a 64-bit
+//!   version of the x86 instruction set.
+//! - **ARM64**: AArch64 or ARM64 is the 64-bit extension of the ARM architecture.
+
+#[cfg(target_arch = "x86_64")]
 mod x86_64;
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(target_arch = "x86_64")]
 pub use x86_64::*;
 
 #[cfg(target_arch = "aarch64")]
