7cb5c9f9eeebfb62df333fd0ec41cc1856a90637
Chao Wu <chaowu@linux.alibaba.com>
2022-01-26 12:23:08 +0800

    db-boot: init db-boot and boot layout constants
    This patch inits db-boot crate and introduce cpu architecture specific
    layout constants
    
    Signed-off-by: Chao Wu <chaowu@linux.alibaba.com>

---
+++ crates/db-boot/src/lib.rs
@@ -0,0 +1,19 @@
+// Copyright 2021-2022 Alibaba Cloud. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#![deny(missing_docs)]
+
+//! CPU architecture specific constants and utilities.
+//!
+//! This crate provides CPU architecture specific constants and utilities to abstract away CPU
+//! architecture specific details from the Dragonball Sandbox or other VMMs.
+
+#[cfg(target_arch = "x86_64")]
+mod x86_64;
+#[cfg(target_arch = "x86_64")]
+pub use x86_64::*;
+
+#[cfg(target_arch = "aarch64")]
+mod aarch64;
+#[cfg(target_arch = "aarch64")]
+pub use aarch64::*;
