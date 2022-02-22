7da009f20f0769e2621bc55be0edca9780e048e9
wllenyj <wllenyj@linux.alibaba.com>
2022-02-22 17:14:19 +0800

    dbs-utils: add metric public components
    Signed-off-by: wllenyj <wllenyj@linux.alibaba.com>

---
+++ crates/dbs-utils/src/lib.rs
@@ -0,0 +1,5 @@
+// Copyright (C) 2022 Alibaba Cloud. All rights reserved.
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod metric;
