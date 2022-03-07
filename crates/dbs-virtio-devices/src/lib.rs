dc189f5654a1efedad379db8685289591f3a63f2
wllenyj <wllenyj@linux.alibaba.com>
2022-03-07 14:49:09 +0800

    Add dbs-virtio-devices crate.
    The `dbs-virtio-devices` provides emulation for virtio devices.
    Currently just an empty crate.
    
    Signed-off-by: wllenyj <wllenyj@linux.alibaba.com>

---
+++ crates/dbs-virtio-devices/src/lib.rs
@@ -0,0 +1,14 @@
+// Copyright 2019-2020 Alibaba Cloud. All rights reserved.
+// Portions Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Portions Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the THIRD-PARTY file.
+
+//! Interfaces and implementations of virtio devices.
+//!
+//! Please refer to [Virtio Specification]
+//! (http://docs.oasis-open.org/virtio/virtio/v1.0/cs04/virtio-v1.0-cs04.html#x1-1090002)
+//! for more information.
+
