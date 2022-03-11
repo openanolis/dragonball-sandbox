c77bfb713742b3f95d442cf3803127c203c0d24c
Chao Wu <chaowu@linux.alibaba.com>
2022-03-11 15:16:34 +0800

    arch: add x86 interrupt definitions
    add LAPIC related support in the x86 arch crate to help get and set
    LAPIC regs
    
    Signed-off-by: Chao Wu <chaowu@linux.alibaba.com>

--- crates/db-arch/src/x86_64/mod.rs
+++ crates/db-arch/src/x86_64/mod.rs
@@ -7,6 +7,8 @@
 pub mod cpuid;
 /// Definitions for x86 Global Descriptor Table
 pub mod gdt;
+/// Definitions for x86 interrupts
+pub mod interrupts;
 /// Definitions for x86 Model Specific Registers(MSR).
 pub mod msr;
 /// Definitions for x86 Registers
