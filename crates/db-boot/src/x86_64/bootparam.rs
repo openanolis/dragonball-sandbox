108927b09544bae4bb4bf9fb2e57b9cb2a6cd59f
Chao Wu <chaowu@linux.alibaba.com>
2022-03-15 18:23:36 +0800

    boot: remove redundant license line
    Signed-off-by: Chao Wu <chaowu@linux.alibaba.com>

--- crates/db-boot/src/x86_64/bootparam.rs
+++ crates/db-boot/src/x86_64/bootparam.rs
@@ -1,5 +1,4 @@
 // Copyright 2021-2022 Alibaba Cloud. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 //
