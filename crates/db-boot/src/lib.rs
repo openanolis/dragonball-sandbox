e9826065fee792ef702e08bc7a3848e589cab70a
jingshan.lx <jingshan@linux.alibaba.com>
2022-03-23 17:14:21 +0800

    db-boot: arm add fdt feature
    Arm adds the fdt feature, which is mainly used to inform the guest
    kernel of device information. In addition, the DeviceType and
    InitrdConfig structures are not specific to fdt, so they are placed in
    lib.rs.
    
    Signed-off-by: jingshan.lx <jingshan@linux.alibaba.com>

--- crates/db-boot/src/lib.rs
+++ crates/db-boot/src/lib.rs
@@ -7,6 +7,9 @@
 //!
 //! This crate provides CPU architecture specific constants and utilities to abstract away CPU
 //! architecture specific details from the Dragonball Sandbox or other VMMs.
+use std::fmt;
+use versionize::{VersionMap, Versionize, VersionizeError, VersionizeResult};
+use versionize_derive::Versionize;
 
 #[cfg(target_arch = "x86_64")]
 mod x86_64;
@@ -17,3 +20,30 @@ pub use x86_64::*;
 mod aarch64;
 #[cfg(target_arch = "aarch64")]
 pub use aarch64::*;
+
+/// Types of devices that can get attached to this platform.
+#[derive(Versionize, Clone, Debug, PartialEq, Eq, Hash, Copy)]
+pub enum DeviceType {
+    /// Device Type: Virtio.
+    Virtio(u32),
+    /// Device Type: Serial.
+    #[cfg(target_arch = "aarch64")]
+    Serial,
+    /// Device Type: RTC.
+    #[cfg(target_arch = "aarch64")]
+    RTC,
+}
+
+/// Type for passing information about the initrd in the guest memory.
+pub struct InitrdConfig {
+    /// Load address of initrd in guest memory
+    pub address: vm_memory::GuestAddress,
+    /// Size of initrd in guest memory
+    pub size: usize,
+}
+
+impl fmt::Display for DeviceType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
