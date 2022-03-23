dd3b6321fee93df90396bd18665ae4646279e413
jingshan.lx <jingshan@linux.alibaba.com>
2022-03-23 15:28:39 +0800

    db-arh: arm add gic and regs dependent library
    This modification mainly involves two aspects:
    
    1. Add gic and cpu regs dependent library.
    
    2. Expose gic and regs modules to external modules.
    
    Signed-off-by: jingshan.lx <jingshan@linux.alibaba.com>

--- crates/db-arch/src/aarch64/mod.rs
+++ crates/db-arch/src/aarch64/mod.rs
@@ -1,4 +1,7 @@
-// Copyright 2021 Alibaba Cloud. All Rights Reserved.
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-//! CPU architecture specific constants, structures and utilities for the `aarch64` architecture.
+/// Module for the global interrupt controller configuration.
+pub mod gic;
+/// Logic for configuring aarch64 registers.
+pub mod regs;
