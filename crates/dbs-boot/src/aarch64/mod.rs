07ff5602201cd0437a94d9bdc00f65216f47ee28
jingshan <jingshan@linux.alibaba.com>
2022-05-04 21:30:57 +0800

    dbs-boot: support vm-fdt crate related error
    This modification mainly replaces the original fdt-related errors with
    the errors of the vm-fdt crate.
    
    Signed-off-by: jingshan <jingshan@linux.alibaba.com>

--- crates/dbs-boot/src/aarch64/mod.rs
+++ crates/dbs-boot/src/aarch64/mod.rs
@@ -3,8 +3,7 @@
 
 //! VM boot related constants and utilities for `aarch64` architecture.
 
-use std::ffi::NulError;
-use std::io;
+use vm_fdt::Error as VmFdtError;
 use vm_memory::GuestMemoryError;
 /// Magic addresses externally used to lay out aarch64 VMs.
 pub mod layout;
@@ -12,21 +11,19 @@ pub mod layout;
 /// FDT is used to inform the guest kernel of device tree information.
 pub mod fdt;
 
-/// Errors thrown while configuring aarch64 system.
+/// Errors thrown while configuring the Flattened Device Tree for aarch64.
 #[derive(Debug)]
 pub enum Error {
-    /// Failed to append node to the FDT.
-    AppendFDTNode(io::Error),
-    /// Failed to append a property to the FDT.
-    AppendFDTProperty(io::Error),
-    /// Syscall for creating FDT failed.
-    CreateFDT(io::Error),
-    /// Failed to obtain a C style string.
-    CstringFDTTransform(NulError),
-    /// Failure in calling syscall for terminating this FDT.
-    FinishFDTReserveMap(io::Error),
+    /// Failure in creating FDT
+    CreateFdt(VmFdtError),
     /// Failure in writing FDT in memory.
     WriteFDTToMemory(GuestMemoryError),
     /// Invalid arguments
     InvalidArguments,
+}
+
+impl From<VmFdtError> for Error {
+    fn from(e: VmFdtError) -> Self {
+        Error::CreateFdt(e)
+    }
 }
